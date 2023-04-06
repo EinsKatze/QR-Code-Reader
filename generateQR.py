fdaa570754e616e4587738633279afbdfdf3f0d3
EinsKatze <82060534+EinsKatze@users.noreply.github.com>
2023-04-06 14:55:46 +0200

    🖼️Switch to SVG & Remove bloated HTML

--- generateQR.py
+++ generateQR.py
@@ -5,11 +5,12 @@ qr = qrcode.QRCode(
     error_correction=qrcode.constants.ERROR_CORRECT_H,
     box_size=4,
     border=1,
+    image_factory=qrcode.image.svg.SvgPathImage,
 )
 
 var = input('What Data should your QR-Code contain?\n> ')
 
 qr.add_data(var)
 qr.make(fit=True)
-img = qr.make_image(fill_color="black", back_color="white").convert('RGB')
-img.save("qr.png")
+img = qr.make_image(fill_color="black", back_color="white")
+img.save("qr.svg")
